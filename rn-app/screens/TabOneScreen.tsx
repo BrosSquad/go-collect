eb39c678788f0f7e3164869139e4780c85fc5576
Jaksa Malisic <jaksa.malisic@gmail.com>
2022-03-12 19:24:29 +0100

    chore(theming): setup ui kitten and branding

--- rn-app/screens/TabOneScreen.tsx
+++ rn-app/screens/TabOneScreen.tsx
@@ -1,17 +1,14 @@
-import { StyleSheet } from 'react-native';
+import { Button, Layout, Text } from '@ui-kitten/components'
+import { StyleSheet } from 'react-native'
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
-
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
-  );
+    <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text category="h1">Scoreboard</Text>
+
+      <Button size="giant">Click me</Button>
+    </Layout>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -29,4 +26,4 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
-});
+})
