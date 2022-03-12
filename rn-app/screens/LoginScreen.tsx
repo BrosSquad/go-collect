a54e1dd668fe331fe1dbbfadb0a1f4016d815b6f
Jaksa Malisic <jaksa.malisic@gmail.com>
2022-03-12 21:44:55 +0100

    feat(auth): implement login page ui

--- rn-app/screens/LoginScreen.tsx
+++ rn-app/screens/LoginScreen.tsx
@@ -1,12 +1,72 @@
-import { Layout, Text } from '@ui-kitten/components'
+import { Button, Divider, Input, Text } from '@ui-kitten/components'
 import React from 'react'
+import { KeyboardAvoidingView, StyleSheet, View } from 'react-native'
+import TrashAnimation from '../components/animations/TrashAnimation'
+import Logo from '../components/Logo'
+import ScreenLayout from '../components/ScreenLayout'
+import { goCollectTheme } from '../go-collect-theme'
 
 const LoginScreen = () => {
   return (
-    <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text category="h1">Login</Text>
-    </Layout>
+    <ScreenLayout>
+      <View style={styles.container}>
+        <View style={styles.logoContainer}>
+          <TrashAnimation />
+          <Logo />
+          <Text category="s1">Please login to continue</Text>
+        </View>
+
+        <KeyboardAvoidingView style={styles.form}>
+          <Input label="Email" placeholder="john@doe.com" />
+          <Input
+            label="Password"
+            placeholder="***********"
+            style={{ marginTop: 16 }}
+          />
+          <Button size="large" style={{ marginTop: 32 }}>
+            Login
+          </Button>
+        </KeyboardAvoidingView>
+
+        <Divider style={{ marginTop: 32 }} />
+
+        <Button
+          size="large"
+          style={{
+            marginTop: 32,
+            backgroundColor: goCollectTheme.googleRed,
+            borderColor: goCollectTheme.googleRed,
+          }}
+        >
+          Continue with Google
+        </Button>
+
+        <Button
+          size="large"
+          style={{
+            marginTop: 16,
+            backgroundColor: goCollectTheme.facebookBlue,
+            borderColor: goCollectTheme.facebookBlue,
+          }}
+        >
+          Continue with Facebook
+        </Button>
+      </View>
+    </ScreenLayout>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'stretch',
+  },
+  logoContainer: {
+    alignSelf: 'center',
+    alignItems: 'center',
+  },
+  form: {
+    marginTop: 48,
+  },
+})
 
 export default LoginScreen
