ef6e206cdca2c9127927921aa989d9b0c03f6351
Dusan Malusev <dmalusev@nanointeractive.com>
2022-03-13 09:56:58 +0100

    fix

--- backend/pkg/models/participants.go
+++ backend/pkg/models/participants.go
@@ -1,8 +1,7 @@
 package models
 
-// TODO: Add Status as Postgres ENUM
-
 type Participant struct {
+	ID      uint64 `gorm:"column:id"`
 	UserID  uint64 `gorm:"column:user_id" json:"user_id,omitempty"`
 	EventID uint64 `gorm:"column:event_id" json:"event_id,omitempty"`
 	Status  string `gorm:"column:status" json:"status,omitempty"`
